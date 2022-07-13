a9d6ac76ee714553978e7b3d127dde48833d4a48
Cavone <danielecavaliere28@gmail.com>
2022-07-13 11:16:16 +0200

    nuova finestra di ricerca

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -33,36 +36,39 @@ public class Controller {
 	}
 		@FXML
     private void checkLogin() throws IOException {
-        Main m = new Main();
-        if(username.getText().toString().equals("user") && password.getText().toString().equals("123")){
-            wronglogin.setText("Success!");
-            m.changeScene("scelta.fxml");
-           // String line = "";
-          //  String splitBy = ",";
-            	/*
-              try (BufferedReader br = new BufferedReader(new FileReader("users.csv"))) {
-                while ((line = br.readLine()) != null)
-                  {
-                    String[] check = line.split(splitBy);
-                    if(check[0] == username && check[1] == password)
-                        return true;  //Entra
-                  }
-                return false;    //Non entra
-            }
-            
-             */
-            
-        }
-
-        else if(username.getText().isEmpty() && password.getText().isEmpty()) {
-            wronglogin.setText("Please enter your data.");
-        }
-
-
-        else {
-            wronglogin.setText("Wrong username or password!");
-        }
-        
+       // Main m = new Main();           	
+           String line = "";
+           String user = username.getText().toString();
+           String pass = password.getText().toString();
+           Boolean done = false;
+           if(username.getText().isEmpty() && password.getText().isEmpty()) {
+               wronglogin.setText("Please enter your data.");
+               return;
+           }
+          try{
+		        BufferedReader br = new BufferedReader(new FileReader("password.csv"));
+		        line = br.readLine();
+		        while ((line = br.readLine()) != null){
+		            String[] check = line.split(",");
+		            System.out.println(check[0] + " " + check[1] +"\n" + pass);
+		            System.out.println(pass.equals(check[1]));
+		
+		            if(user.equals(check[0]) && pass.equals(check[1])) {
+		                wronglogin.setText("Success!");
+		                done = true;
+		                Main m = new Main();
+		                m.changeScene("scelta.fxml");
+		                break;
+		                
+		
+		            }
+		        }
+          } catch (FileNotFoundException e) {
+            e.printStackTrace();
+          }
+          if(!done) {
+                wronglogin.setText("Wrong username or password. Retry.");
+          }
     }
 	@FXML
 	private void userLogout(ActionEvent event) throws IOException {
